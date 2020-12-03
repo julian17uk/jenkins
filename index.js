6cd340e5c6ea5fdb99a42e23ba960623216052d3
juliankarnik <julian.karnik@ecs-digital.co.uk>
2020-12-03 11:55:06 +0000

    debug with error

--- index.js
+++ index.js
@@ -7,5 +7,25 @@ app.get("/", function(req, res) {
 });
 
 app.listen(port, function() {
-  console.log("port is listening to port no 3090");
+  console.log("port is listening to port no 3090")));
 });
+
+function addTwoNumbers(x, y) {
+  return x + y;
+}
+
+function testAddTwoNumbers() {
+  var x = 5;
+  var y = 1;
+  var sum1 = x + y;
+  var sum2 = addTwoNumbers(x, y);
+
+  console.log("addTwoNumbers() should return the sum of its two parameters.");
+  console.log("Expect " + sum1 + " to equal " + sum2 + ".");
+
+  if (sum1 === sum2) return console.log("Passed.");
+
+  console.log("Failed.");
+}
+
+testAddTwoNumbers();
