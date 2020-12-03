fc40f5d1813d631fe56b282031538b8c5774e018
juliankarnik <julian.karnik@ecs-digital.co.uk>
2020-12-03 11:26:02 +0000

    debug: error added

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var express = require("express");
 var app = express();
-const port = 3080;
+const port = 3090;
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/home.html");
 });
 
 app.listen(port, function() {
-  console.log("port is listening to 3080");
+  console.log("port is listening to port no 3090")));
 });
