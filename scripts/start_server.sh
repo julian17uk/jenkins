8cb53e9c745b50a18f569dca4abf3243ccbf872f
juliankarnik <julian.karnik@ecs-digital.co.uk>
2020-12-04 12:10:57 +0000

    fix bug

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 # stop any old running servers 
 echo $PATH
-# sudo ~/.nvm/versions/node/v15.3.0/bin/node --version
-# npm --version
-# npm ls express
-# sudo ~/.nvm/versions/node/v15.3.0/bin/node app/index.js
-# node app/index.js
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install node
-npm install express
 npm --version
 npm ls express
-node /var/www/html/index.js
+node app/index.js
