6d1ea4af30bafd4759f4a4efb3ba83677e647b79
juliankarnik <julian.karnik@ecs-digital.co.uk>
2020-12-04 12:40:02 +0000

    fix bug

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 # stop any old running servers 
 echo $PATH
+#npm --version
+#npm ls express
+#node app/index.js
+
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node
+npm install express
 npm --version
 npm ls express
-node app/index.js
+node /home/ec2-user/app/index.js
