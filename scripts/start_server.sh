3dc534ea7217970746dbe8ddd1276ec4c4ac6d30
juliankarnik <julian.karnik@ecs-digital.co.uk>
2020-12-04 11:44:39 +0000

    fix: install npm, express

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 # stop any old running servers 
 echo $PATH
-sudo ~/.nvm/versions/node/v15.3.0/bin/node --version
+# sudo ~/.nvm/versions/node/v15.3.0/bin/node --version
 # npm --version
 # npm ls express
-sudo ~/.nvm/versions/node/v15.3.0/bin/node app/index.js
+# sudo ~/.nvm/versions/node/v15.3.0/bin/node app/index.js
 # node app/index.js
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node
+npm install express
+npm --version
+npm ls express
+node app/index.js
